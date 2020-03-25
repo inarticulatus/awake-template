a699d787e1379c87f5704ee6e6ab4d932fc1c75a
Utkarsh Tripathi <utkarsh1999tripathi@gmail.com>
2020-03-26 01:36:15 +0530

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -9,8 +9,8 @@ import theCMS from '../cms/netlify/cms'
  * General Site configurations
  */
 export default {
-  siteName: 'Aamir Photography',
-  title: 'Aamir Photography',
+  siteName: 'Prakhar Photography',
+  title: 'Prakhar Photography',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
